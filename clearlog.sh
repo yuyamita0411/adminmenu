55077fcc594b5ab9e95a18fca4b8cc71dd1c32cc
yuyamita0411 <kwansei0411@gmail.com>
2024-03-21 18:08:51 +0900

    bk

--- clearlog.sh
+++ clearlog.sh
@@ -1,3 +1,5 @@
 #pycacheのフォルダを削除する。
-git add . && git commit -m "bk" && git push origin main &&
 find ./* | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
+git add .
+git commit -m "bk"
+git push origin main
