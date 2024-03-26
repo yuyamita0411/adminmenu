990c11c53470d8bffd9019f14a5c6e804209eb5a
yuyamita0411 <kwansei0411@gmail.com>
2024-03-26 12:22:18 +0900

    bk

--- API/app/app.py
+++ API/app/app.py
@@ -16,109 +16,51 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(f'{basedir}translation_tool', '.env'))
 
-@app.route(os.getenv("VUE_APP_fileEndpoint"), methods=['POST'])
-def save_json():
+def get_json_data():
     data = request.get_json()
-    if Function.fileExists(data["filePath"])["status_code"] != 200:
-        os.makedirs(os.path.dirname(data["filePath"]), exist_ok=True)
-        with open(data["filePath"], 'w', encoding='utf-8') as json_file:
-            json.dump(data["fileData"], json_file, ensure_ascii=False, indent=4)
-        return jsonify({'message': 'JSON data saved successfully'}), 200
+    if data is None:
+        return None, jsonify({'error': 'No JSON data received'}), 400
+    return data, None, None
 
-    if data["fileData"] is None:
-        return jsonify({'error': 'No JSON data received'}), 400
-    # JSONファイルに保存、ensure_ascii=Falseを指定して日本語がエスケープされないようにする
+def file_exists(filePath):
+    if not os.path.exists(os.path.dirname(filePath)):
+        os.makedirs(os.path.dirname(filePath), exist_ok=True)
+    return os.path.exists(filePath)
+
+def save_json_data(filePath, fileData):
     try:
-        with open(data["filePath"], 'w', encoding='utf-8') as json_file:
-            json.dump(data["fileData"], json_file, ensure_ascii=False, indent=4)
+        with open(filePath, 'w', encoding='utf-8') as json_file:
+            json.dump(fileData, json_file, ensure_ascii=False, indent=4)
         return jsonify({'message': 'JSON data saved successfully'}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-@app.route(os.getenv("VUE_APP_fileReadEndpoint"), methods=['POST'])
-def list_directory_contents_Data():
-    data = request.get_json()
-    if Function.fileExists(data["filePath"])["status_code"] != 200:
-        return {}
-
-    with open(data["filePath"], 'r', encoding='utf-8') as file:
-        data = file.read()
-        return data
-
-@app.route(os.getenv("VUE_APP_fileDirectory"), methods=['POST'])
-def list_directory_contents():
-    data = request.get_json()
-    dirList = []
-    if Function.fileExists(data["filePath"])["status_code"] != 200:
-        return {}
+def read_json_data(filePath):
     try:
-        # ディレクトリ内の項目をリストアップ
-        contents = os.listdir(data["filePath"])
-        for item in contents:
-            dirList.append(item)
-        return dirList
+        with open(filePath, 'r', encoding='utf-8') as file:
+            return json.load(file), 200
     except FileNotFoundError:
-        return f"The directory '{data['filePath']}' does not exist."
-    except PermissionError:
-        return f"Permission denied to access '{data['filePath']}'."
+        return {'error': f"The file '{filePath}' does not exist."}, 404
+    except Exception as e:
+        return {'error': str(e)}, 500
 
-@app.route(os.getenv("VUE_APP_filetitle"), methods=['POST'])
-def make_file_title_json_data():
-    data = request.get_json()
-    dirDict = {}
-    if data["filePath"] is None:
-        print("ファイルパスなし")
-        return {}
+def list_directory(filePath):
     try:
-        for url in data["filePath"]:
-            jsondir = f'{os.getenv("VUE_APP_articleDirPath")}/{url}/language/jp/index.json'
-            if Function.fileExists(jsondir)["status_code"] == 200:
-                with open(jsondir, 'r', encoding='utf-8') as file:
-                    jsondata = json.load(file)
-                    if "pagetitle" in jsondata:
-                        dirDict[url] = jsondata["pagetitle"]
-
+        return os.listdir(filePath), 200
     except FileNotFoundError:
-        return f"The directory '{data['filePath']}' does not exist."
+        return {'error': f"The directory '{filePath}' does not exist."}, 404
     except PermissionError:
-        return f"Permission denied to access '{data['filePath']}'."
-    return dirDict
+        return {'error': f"Permission denied to access '{filePath}'."}, 403
 
-@app.route(os.getenv("VUE_APP_fileTranslateEndpoint"), methods=['POST'])
-def translate_json():
-    data = request.get_json()
-    if Function.fileExists(data["filePath"])["status_code"] != 200:
-        return {}
-    single(data["filePath"])
-    return {"status_code": 200}
+def make_directory(path):
+    os.makedirs(path, exist_ok=True)
 
-@app.route(os.getenv("VUE_APP_rebaseDirEndpoint"), methods=['POST'])
-def make_dir_from_json():
-    data = request.get_json()
-    dirlits = os.listdir(f'{os.getenv("VUE_APP_articleDirPath")}')
-
-    for dir in dirlits:
-        if dir not in data["filePath"]:
-            remove_directory_with_contents(f'{os.getenv("VUE_APP_articleDirPath")}/{dir}')
-
-    for filePath in data["filePath"]:
-        if filePath and filePath not in dirlits:
-            makePath(f'{os.getenv("VUE_APP_articleDirPath")}/{filePath}')
-            makePath(f'{os.getenv("VUE_APP_articleDirPath")}/{filePath}/language')
-            makePath(f'{os.getenv("VUE_APP_articleDirPath")}/{filePath}/language/jp')
-            with open(f'{os.getenv("VUE_APP_articleDirPath")}/{filePath}/language/jp/index.json', 'w') as file:
-                json.dump({"pagetitle": "タイトルなし"}, file, ensure_ascii=False, indent=4)
-    return {}
-
-
-# 空のディレクトリを削除する関数
 def remove_empty_directory(directory_path):
     try:
         os.rmdir(directory_path)
     except OSError as error:
         print(f"ディレクトリ {directory_path} の削除に失敗しました。ディレクトリが空でないか、存在しない可能性があります。")
 
-# 内容を含むディレクトリを削除する関数
 def remove_directory_with_contents(directory_path):
     try:
         shutil.rmtree(directory_path)
@@ -133,6 +75,78 @@ def makePath(dirpath):
         print(f"ディレクトリ'{dir_path}'が生成されました。")
     else:
         print(f"ディレクトリ'{dir_path}'は既に存在します。")
+@app.route(os.getenv("VUE_APP_fileReadEndpoint"), methods=['POST'])
+def list_directory_contents_Data():
+    data = request.get_json()
+    if Function.fileExists(data["filePath"])["status_code"] != 200:
+        return {}
+
+    with open(data["filePath"], 'r', encoding='utf-8') as file:
+        data = file.read()
+        return data
+
+
+
+@app.route(os.getenv("VUE_APP_fileEndpoint"), methods=['POST'])
+def save_json():
+    data, error, status_code = get_json_data()
+    if error:
+        return error, status_code
+    if not file_exists(data["filePath"]):
+        print(data["filePath"])
+        print(data["fileData"])
+        return save_json_data(data["filePath"], data["fileData"])
+    if "fileData" not in data or data["fileData"] is None:
+        return jsonify({'error': 'No JSON data provided'}), 400
+    return save_json_data(data["filePath"], data["fileData"])
+
+@app.route(os.getenv("VUE_APP_fileDirectory"), methods=['POST'])
+def list_directory_contents():
+    data, error, status_code = get_json_data()
+    if error:
+        return error, status_code
+    response, status_code = list_directory(data["filePath"])
+    return jsonify(response), status_code
+
+@app.route(os.getenv("VUE_APP_filetitle"), methods=['POST'])
+def make_file_title_json_data():
+    data, error, status_code = get_json_data()
+    if error:
+        return error, status_code
+    dirDict = {}
+    for url in data["filePath"]:
+        jsondir = f'{os.getenv("VUE_APP_articleDirPath")}/{url}/language/jp/index.json'
+        if os.path.exists(jsondir):
+            jsondata, _ = read_json_data(jsondir)
+            if "pagetitle" in jsondata:
+                dirDict[url] = jsondata["pagetitle"]
+    return jsonify(dirDict), 200
+
+@app.route(os.getenv("VUE_APP_rebaseDirEndpoint"), methods=['POST'])
+def make_dir_from_json():
+    data, error, status_code = get_json_data()
+    if error:
+        return error, status_code
+    dirlits = os.listdir(f'{os.getenv("VUE_APP_articleDirPath")}')
+    for dir in dirlits:
+        if dir not in data["filePath"]:
+            remove_directory_with_contents(f'{os.getenv("VUE_APP_articleDirPath")}/{dir}')
+    for filePath in data["filePath"]:
+        if filePath:
+            dirPath = f'{os.getenv("VUE_APP_articleDirPath")}/{filePath}'
+            make_directory(dirPath)
+            make_directory(f'{dirPath}/language')
+            make_directory(f'{dirPath}/language/jp')
+            save_json_data(f'{dirPath}/language/jp/index.json', {"pagetitle": "タイトルなし"})
+    return jsonify({}), 200
+
+@app.route(os.getenv("VUE_APP_fileTranslateEndpoint"), methods=['POST'])
+def translate_json():
+    data = request.get_json()
+    if Function.fileExists(data["filePath"])["status_code"] != 200:
+        return {}
+    single(data["filePath"])
+    return {"status_code": 200}
 
 if __name__ == '__main__':
     app.run(debug=True)
