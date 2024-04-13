b836eb8421a417025eda3a7a4497b5d4553a3a56
yuyamita0411 <72258539+yuyamita0411@users.noreply.github.com>
2024-04-13 12:08:15 +0900

    Delete gitpush.sh

--- gitpush.sh
+++
@@ -1,7 +0,0 @@
-#pycacheのフォルダを削除する。
-find ./* | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
-git add .
-git commit -m "bk"
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/adminmenu
-git push origin main
